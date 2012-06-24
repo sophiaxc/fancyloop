f3391df01bbcb4fd1654b2e72c6ab230fee34c31
Sophia Cui <sophiaxc@gmail.com>
2012-06-24 00:41:45 -0700

    Finish remaining user controller actions.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    name     "Sophia Cui"
-    email    "sophia@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,7 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:admin) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password) }
@@ -26,6 +27,13 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
