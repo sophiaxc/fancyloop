b47f14630524cbacd27a65e798388183041bb42d
Sophia Cui <sophiaxc@gmail.com>
2012-06-23 22:46:50 -0700

    Finish user sign ups & add SSL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Fancyloop::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Sophia Cui"
+    email    "sophia@example.com"
+    password "foobar"
+  end
+end
