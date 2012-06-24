79182e288cd216ed0434b8b279048c65cdd312dc
Sophia Cui <sophiaxc@gmail.com>
2012-06-23 19:15:03 -0700

    Filling out application layout.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,19 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
-
-    it "should have the content 'FancyLoop'" do
-      visit '/static_pages/home'
-      page.should have_content('FancyLoop')
-    end
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "FancyLoop | Home")
-    end
+    before { visit root_path }
+    it { should have_selector('h1', text: 'FancyLoop') }
+    it { should have_selector('title',
+                        :text => "FancyLoop | Home") }
   end
 
   describe "About page" do
-
-    it "should have the content 'About FancyLoop'" do
-      visit '/static_pages/about'
-      page.should have_content('About FancyLoop')
-    end
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "FancyLoop | About")
-    end
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About FancyLoop') }
+    it { should have_selector('title',
+                        :text => "FancyLoop | About") }
   end
 end
