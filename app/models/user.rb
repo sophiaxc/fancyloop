d507ceae5de04e0b821cd101f52915d7ed34c7b1
Sophia Cui <sophiaxc@gmail.com>
2012-06-23 21:29:23 -0700

    Create basic user model.

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  has_secure_password
+
+  attr_accessible :name, :email, :password
+
+  before_save { |user| user.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+end
