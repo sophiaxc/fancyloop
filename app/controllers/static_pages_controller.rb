23e519ed76a383b8b47c7ec1b7ad9e262008a114
Sophia Cui <sophiaxc@gmail.com>
2012-07-01 22:06:40 -0700

    Prettify non-signedin homepage.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @user = User.new if not signed_in?
     @project = current_user.projects.build if signed_in?
     @projects = current_user.projects.paginate(page: params[:page], per_page: 5) if signed_in?
   end
