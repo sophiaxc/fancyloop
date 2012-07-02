04e591bbe7eb614020564c94cf117a521d33728d
Sophia Cui <sophiaxc@gmail.com>
2012-07-01 21:28:22 -0700

    Fix sign up view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to FancyLoop!"
-      redirect_to @user
+      redirect_to root_path
     else
       render 'new'
     end
